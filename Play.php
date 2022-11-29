340e0c8f7b2d36df1f65a9f9722091aadf9b0c62
ramzi <ramzi446@hotmail.com>
2022-11-29 10:33:04 +0100

    Initial commit

---
+++ Play.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace play;
+
+class Ship
+{
+public $name;
+public $weponPower;
+public $jediFactor;
+public $strenth;
+
+
+}
+$myShip = new Ship();
+$myShip->name = 'helloship';
+$myShip->sayHello();
